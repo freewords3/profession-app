d0ac73a19cbdbad1f8c37f88136d78d81ed888d1
freewords3 <55410396+freewords3@users.noreply.github.com>
2021-03-10 21:41:24 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {Provider} from 'react-redux';
+import store from 'store'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
+
